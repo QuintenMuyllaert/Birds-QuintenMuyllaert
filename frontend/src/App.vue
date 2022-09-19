e5ccff5efdcb73904de972dab1b2270b76cc20e8
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-09-19 23:10:17 +0200

    🔧 Configure unocss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+	<h1 class="text-red-500">test</h1>
+	<h2 class="text-green-200">test2</h2>
+	<h3>test3</h3>
+</template>
 
 <style></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,7 @@
 import { App as VueApp, createApp } from "vue";
 import App from "./App.vue";
+import "@unocss/reset/tailwind.css";
+import "uno.css";
 
 const app: VueApp = createApp(App);
 app.mount("#app");
