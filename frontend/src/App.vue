1d28d03d71b3ecfdc225668e4e92e5c41bf5cee9
Quinten Muyllaert <72012305+QuintenMuyllaert@users.noreply.github.com>
2022-09-20 10:34:57 +0200

    💄 Added root styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="text text-6xl text-red-500">
-		<router-view></router-view>
-	</div>
+	<router-view></router-view>
 </template>
 
-<style></style>
+<style lang="scss">
+:root {
+	--color-primary: #a1e8af;
+	--color-secondary: #94c595;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	padding-inline: 4rem;
+}
+</style>
